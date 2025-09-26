17f70a978c45cccb415263842177040668ec2685
Phil Pluckthun <phil@kitten.sh>
2025-09-26 13:28:23 +0100

    cli: Remove `hot: true` from `transformOptions` in `metroLoadModuleContents`

--- packages/@expo/cli/src/start/server/metro/MetroBundlerDevServer.ts
+++ packages/@expo/cli/src/start/server/metro/MetroBundlerDevServer.ts
@@ -605,7 +605,6 @@ export class MetroBundlerDevServer extends BundlerDevServer {
 
     const transformOptions: TransformInputOptions = {
       dev: expoBundleOptions.dev ?? true,
-      hot: true,
       minify: expoBundleOptions.minify ?? false,
       type: 'module',
       unstable_transformProfile:
